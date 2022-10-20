233fd3ed15f75ace98c493de9370a182a38a3e74
davidtea <davidtea1953@gmail.com>
2022-10-20 16:50:22 -0500

    Se recorrio el json en busca de la etiqueta deseada

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var params ={
     Image: {
         S3Object: {
             Bucket: "imgdecomparacion",
-            Name: "laptopAndres.jpeg"
+            Name: "codigo.png"
         }
     },
     MaxLabels: 5,
@@ -30,8 +30,34 @@ rekognition.detectLabels(params, function( err , data){
     else{
         let datas = JSON.parse(JSON.stringify(data));
 
-        // console.log(data);
-        console.log(data.Labels[0]);
+        //console.log(data);
+        //console.log(datas.Labels[0]);
+
+     
+
+        for(x of datas.Labels){
+
+            
+            
+
+            if (x.Name == "Laptop" ) {
+
+                if (x.Confidence > 98) {
+                    
+                    console.log(x.Confidence, x.Name);
+                }
+                else{
+                    console.log("no tiene la suficiente confianza")
+                }
+
+                
+            }
+            
+               
+            
+           
+        }
+        console.log("No es una laptop");
 
     }
 
